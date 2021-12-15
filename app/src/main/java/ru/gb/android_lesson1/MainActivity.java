80201ed7c6c8bc01007c80457c1a2fcef6928548
sdltsky <sedletskiyna@mail.ru>
2021-12-16 02:04:33 +0500

    Добавил элемент CalendarView. Сделал кнопку переключения на вторую активити

--- app/src/main/java/ru/gb/android_lesson1/MainActivity.java
+++ app/src/main/java/ru/gb/android_lesson1/MainActivity.java
@@ -2,12 +2,28 @@ package ru.gb.android_lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button switchActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchActivity = findViewById(R.id.switch_activity_button);
+        switchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.second_activity);
+            }
+        });
+
+
     }
 }
